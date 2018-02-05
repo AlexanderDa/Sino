70db98b720121f2768d88eb103ee71bd6c32d6c2
Alexander Bonilla <aldaboad@gmail.com>
2018-02-05 09:01:58 -0500

    Agregar la descripcion a los quimestres

--- Sql/Values.sql
+++ Sql/Values.sql
@@ -2,7 +2,7 @@ insert into administrador(usuario,clave,nombre,apellido)
 	values('admin',md5('admin'),'admin','admin') ;
 
 
-insert into periodo(fecha_inicio,fecha_fin,director,subdirector,cordinador) values
+insert into periodo(fecha_inicio,fecha_fin,director,subdirector,coordinador) values
 	('2016-9-1','2017-7-30','Kateryne Pazmino','Catalina Itriago','María José quintanilla'),
 	('2017-9-1','2018-7-30','Juan Palacios','Catalina Itriago','María José quintanilla');	
 
@@ -46,14 +46,14 @@ insert into curso(periodo, docente, materia, grado, paralelo) values
 
 --NOTAS DE LENLUAJE DE CAGUANO ERICK
 insert into ciclo(curso,alumno) values(1,'065838557-6');
-insert into quimestre(ciclo,quimestral) values (1,9);
+insert into quimestre(ciclo,descripcion,quimestral) values (1,'PRIMER QUIMESTRE',9);
 
 insert into parcial(quimestre, descripcion, tarea, individual, grupal, nota_parcial) 
 	values(1,'PRIMER PARCIAL',7,8,10,8),
 			(1,'SEGUNDO PARCIAL',10,8,7,8),
 			(1,'TERCER PARCIAL',7,8,8,8);
 			
-insert into quimestre(ciclo,quimestral) values (1,9);
+insert into quimestre(ciclo,descripcion,quimestral) values (1,'SEGUNDO QUIMESTRE',9);
 insert into parcial(quimestre, descripcion, tarea, individual, grupal, nota_parcial) 
 	values	(2,'PRIMER PARCIAL',10,9,7,8),
 			(2,'SEGUNDO PARCIAL',7,8,8,9),
@@ -65,13 +65,13 @@ insert into parcial(quimestre, descripcion, tarea, individual, grupal, nota_parc
 --NOTAS DE LENLUAJE DE CAMACHO MATEO 
 insert into ciclo(curso,alumno) values(2,'056437659-4');
 
-insert into quimestre(ciclo,quimestral) values (2,10);
+insert into quimestre(ciclo,descripcion,quimestral) values (2,'PRIMER QUIMESTRE',10);
 insert into parcial(quimestre, descripcion, tarea, individual, grupal, nota_parcial) 
 	values(3,'PRIMER PARCIAL',8,5,10,7),
 			(3,'SEGUNDO PARCIAL',8,9,7,8),
 			(3,'TERCER PARCIAL',8,9,8,8);
 			
-insert into quimestre(ciclo,quimestral) values (2,9);
+insert into quimestre(ciclo,descripcion,quimestral) values (2,'SEGUNDO QUIMESTRE',9);
 insert into parcial(quimestre, descripcion, tarea, individual, grupal, nota_parcial) 
 	values	(4,'PRIMER PARCIAL',10,9,7,8),
 			(4,'SEGUNDO PARCIAL',8,9,8,7),
@@ -82,14 +82,14 @@ insert into parcial(quimestre, descripcion, tarea, individual, grupal, nota_parc
 
 --NOTAS DE LENLUAJE DE CARPINTERO LUIS 
 insert into ciclo(curso,alumno) values(3,'041296490-8');
-insert into quimestre(ciclo,quimestral) values (3,8);
+insert into quimestre(ciclo,descripcion,quimestral) values (3,'PRIMER QUIMESTRE',8);
 
 insert into parcial(quimestre, descripcion, tarea, individual, grupal, nota_parcial) 
 	values(5,'PRIMER PARCIAL',9,10,10,9),
 			(5,'SEGUNDO PARCIAL',9,8,7,8),
 			(5,'TERCER PARCIAL',9,10,8,8);
 			
-insert into quimestre(ciclo,quimestral) values (3,8);
+insert into quimestre(ciclo,descripcion,quimestral) values (3,'SEGUNDO QUIMESTRE',8);
 insert into parcial(quimestre, descripcion, tarea, individual, grupal, nota_parcial) 
 	values	(6,'PRIMER PARCIAL',9,7,7,8),
 			(6,'SEGUNDO PARCIAL',7,7,8,8),
