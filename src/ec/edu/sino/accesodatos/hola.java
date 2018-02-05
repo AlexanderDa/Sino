9ea58d0af033320b7d988660a6283a2d9e3f2283
Usuario <Usuario@FI-LABC06-PC07>
2018-02-05 15:58:38 -0500

    borrar esto

---
+++ src/ec/edu/sino/accesodatos/hola.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.sino.accesodatos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class hola {
+    public static void main(String[] args) {
+     
+        System.out.println("ec.edu.sino.accesodatos.hola.main()");
+        
+    }    
+    
+    
+}
