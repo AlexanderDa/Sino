c9161e67622cc1f466e99f762e79a20976229bae
Alexander Bonilla <aldaboad@gmail.com>
2018-02-05 21:40:27 -0500

    Creacion de la capa de contrato Materia

---
+++ src/ec/edu/sino/dao/contrato/IMateria.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.sino.dao.contrato;
+
+import ec.edu.sino.negocios.entidades.Materia;
+import javafx.collections.ObservableList;
+
+/**
+ *
+ * @author alexander
+ */
+public interface IMateria {
+
+    void loginAdmin();
+
+    void loginProfesor();
+
+    int insertar(Materia materia) throws Exception;
+
+    int modificar(Materia materia) throws Exception;
+
+    int eliminar(Materia materia) throws Exception;
+
+    Materia obtener(String cedula) throws Exception;
+
+    ObservableList<Materia> obtener() throws Exception;
+}
