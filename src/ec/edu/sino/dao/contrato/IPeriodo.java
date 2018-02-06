36689b3dd59cfdf7f541983efd193bb16dcc120b
Alexander Bonilla <aldaboad@gmail.com>
2018-02-05 21:41:27 -0500

    Creacion de la capa de contrato Periodo

---
+++ src/ec/edu/sino/dao/contrato/IPeriodo.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.sino.dao.contrato;
+
+import ec.edu.sino.negocios.entidades.Periodo;
+import javafx.collections.ObservableList;
+
+/**
+ *
+ * @author alexander
+ */
+public interface IPeriodo {
+
+    void loginAdmin();
+
+    void loginProfesor();
+
+    int insertar(Periodo periodo) throws Exception;
+
+    int modificar(Periodo periodo) throws Exception;
+
+    int eliminar(Periodo periodo) throws Exception;
+
+    Periodo obtener(String id) throws Exception;
+
+    ObservableList<Periodo> obtener() throws Exception;
+}
