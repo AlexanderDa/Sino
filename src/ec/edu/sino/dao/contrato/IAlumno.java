8e58269eb8aefd6e0d16e28af1c1b560c53642c9
Alexander Bonilla <aldaboad@gmail.com>
2018-02-05 21:39:06 -0500

    Creacion de la capa de contrato Alumno

---
+++ src/ec/edu/sino/dao/contrato/IAlumno.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.sino.dao.contrato;
+
+import ec.edu.sino.negocios.entidades.Alumno;
+import javafx.collections.ObservableList;
+
+/**
+ *
+ * @author alexander
+ */
+public interface IAlumno {
+
+    void loginAdmin();
+
+    void loginProfesor();
+
+    int insertar(Alumno alumno) throws Exception;
+
+    int modificar(Alumno alumno) throws Exception;
+
+    int eliminar(Alumno alumno) throws Exception;
+
+    Alumno obtener(String cedula) throws Exception;
+
+    ObservableList<Alumno> obtener() throws Exception;
+}
